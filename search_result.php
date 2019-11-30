4c36560b00ef91768a3e5cef709beb06f61ebaff
dominhhien2808 <58357750+dominhhien2808@users.noreply.github.com>
2019-11-30 16:12:52 +0700

    Add files via upload
    add tập tin PHP

---
+++ search_result.php
@@ -0,0 +1,61 @@
+<?php
+	require_once 'init.php';
+	require_once 'function.php';
+	//xu ly logic o day
+	$page = 'index';
+	$posts = findAllPosts();
+	if(!$currentUser)
+    {
+        header('Location: log-sin.php');
+        exit(0);
+    }
+?>
+<?php include 'header.php'; ?>
+
+
+
+<?php if($currentUser): ?>				
+		      	<li class="nav-item <?php echo $page === 'posts' ? 'active' :'' ?>">
+		        <a class="nav-link" href="posts.php">Đăng trạng thái</a>
+		      </li>
+<?php endif; ?>   
+	<?php 
+		
+		if(isset($_GET['keyword']) && !empty($_GET['keyword']) ){
+			echo "kết quả tìm kiếm cho :" . $_GET['keyword'];
+			$s = trim(htmlspecialchars(addslashes($_GET['keyword'])));// lấy từ khóa từ ô search...
+    	$db = new PDO('mysql:host=localhost;dbname=dack;charset=utf8', 'root', ''); 
+   	 	$stmt = $db->prepare("SELECT * FROM users WHERE fullname LIKE '%$s%' or email LIKE '%$s%' ORDER BY id ");
+    	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+    	$stmt->execute();
+    	$resultSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo "<table width = '600' boder ='5' style='margin-top:40px'> ";
+    echo " <h1 style='margin-top:40px'> Có Phải Bạn Đang Tìm Kiếm </h1>";
+    echo "<tr>";
+    echo"<td><u> Tên: </u></td> ";
+	echo"<td><u> Email: </u> </td> ";
+	echo"<td><u> id: </u> </td> ";
+    echo "</tr>";
+        $i = 0;
+        foreach ($resultSet as $row) {
+        $ten = $row['fullname'];
+        $email = $row['email'];
+		$id = $row['id'];
+
+        echo "<tr>";
+        echo" <a href='google.com'> <td> $ten </td> </a>";
+		echo"<td> $email </td> ";
+		echo" <td> $id </td>";
+        echo "</tr>";
+        $i++;
+        }
+     echo "</table> "   ;
+if($i == 0) {echo '<div> tìm kiếm không thành công </div>';}
+		}
+
+	?>
+
+</body>
+
+
+
